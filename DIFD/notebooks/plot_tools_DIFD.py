16fa0c3f402afc07ea662d4805ab2dd8c704fbe7
CarolineHalllin <64251347+CarolineHalllin@users.noreply.github.com>
2023-03-02 09:46:18 +0100

    update of modelfiles (#7)
    * update of modelfiles
    
    There was a conflict at the previous merge so the modelfiles were not updated properly
    
    * Update notebook 3 and model tools

--- DIFD/notebooks/plot_tools_DIFD.py
+++ DIFD/notebooks/plot_tools_DIFD.py
@@ -5,15 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors
 from matplotlib.animation import FuncAnimation
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-
-# import colormap
-colors = np.loadtxt('cmap_DIFD.txt')
-cmap_DIFD = matplotlib.colors.ListedColormap(colors)
 
 
 def plot_topo(ncfile, change=False, figsize=(10,5),time_index_start=0, time_index_end=-1, ax=None):
-    '''Plot bathymetries or the erosion/deposition from an AeoLiS result file
+    '''Plot topography or the erosion/deposition from an AeoLiS result file
     
     Parameters
     ----------
@@ -23,8 +20,10 @@ def plot_topo(ncfile, change=False, figsize=(10,5),time_index_start=0, time_inde
       Plot bathymetric change rather than actual bathymetry
     figsize : 2-tuple, optional
       Dimensions of resulting figure
-    time_index : int, optional
-      Index of time dimension to plot [default: -1]
+    time_index_start : int
+      Index of first (initial) time dimension to plot [default: 0]
+    time_index_stop : int
+      Index of last (final) time dimension to plot [default: -1]
     ax : matplotlib.axes.SubplotAxis, optional
       Axis used for plotting, if not given a new figure is created
       
@@ -38,204 +37,179 @@ def plot_topo(ncfile, change=False, figsize=(10,5),time_index_start=0, time_inde
     with netCDF4.Dataset(ncfile, 'r') as ds:
         
         # get spatial dimensions and bed levels
-        x = ds.variables['x'][:,:]
-        y = ds.variables['y'][:,:]
-        zb = ds.variables['zb'][...]
+        x = np.squeeze(ds.variables['x'][:,:])
+        zb = np.squeeze(ds.variables['zb'][...])
 
-        
         # create figure
         if ax is None:
             fig, ax = plt.subplots(figsize=figsize)
         
         # plot bed levels and bed level change
         if not change:
-            p1 = ax.plt(x, zb[time_index_start,:,:], 'k-')
-            p2 = ax.plot(x, zb[time_index_end,:,:], 'r-')
-            plt.title("Initial and final profiles")
-            plt.xlabel("Cross-Shore distance (m)")
-            plt.ylabel("Elevation rel MSL (m)")
-
-        # else:
-        #     pickup = pickup.sum(axis=-1)             # sum over fractions
-        #     pickup = np.cumsum(pickup, axis=0)       # cummulative sum in time
-        #     dz = -pickup / (2650. * .6)               # convert from kg/m2 to m3/m2
-        #     p = ax.pcolormesh(y, x, dz[time_index,:,:], cmap='bwr_r', vmin=-2, vmax=2)
-        #     cb = fig.colorbar(p, shrink=.7)
-        #     cb.set_label('bed level change [m]')
-
-        # ax.contour(y, x, zb[0,:,:], [0.], colors=['k'])
-            
-        # ax.set_aspect('equal', adjustable='box')
-        # ax.invert_yaxis()
-            
-        # ax.set_xlabel('alongshore distance [m]')
-        # ax.set_ylabel('cross-shore distance [m]')
-
-    return ax
+            p1 = ax.plot(x, zb[time_index_start,:], 'k-', label='Initial')
+            p2 = ax.plot(x, zb[time_index_end,:], 'r-', label = 'Final')
+            plt.title(['Initial and final profiles'])
+            ax.set_xlabel('Cross-shore distance [m]')
+            ax.set_ylabel('Elevation rel MSL [m]')
+            ax.legend()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#%%
-#*******Bas scripts***** delete when done
-
-def plot_bathymetry(ncfile, change=False, figsize=(10,5), time_index=-1, ax=None):
-    '''Plot bathymetries or the erosion/deposition from an AeoLiS result file
-    
-    Parameters
-    ----------
-    ncfile : str
-      Path to AeoLiS result file
-    change : bool
-      Plot bathymetric change rather than actual bathymetry
-    figsize : 2-tuple, optional
-      Dimensions of resulting figure
-    time_index : int, optional
-      Index of time dimension to plot [default: -1]
-    ax : matplotlib.axes.SubplotAxis, optional
-      Axis used for plotting, if not given a new figure is created
-      
-    Returns
-    -------
-    ax : matplotlib.axes.SubplotAxis
-      plot axes objects
-      
-    '''
-    
-    with netCDF4.Dataset(ncfile, 'r') as ds:
-        
-        # get spatial dimensions and bed levels
-        x = ds.variables['x'][:,:]
-        y = ds.variables['y'][:,:]
-        zb = ds.variables['zb'][...]
-        pickup = ds.variables['pickup_sum'][...]
-        
-        # create figure
-        if ax is None:
-            fig, ax = plt.subplots(figsize=figsize)
-        
-        # plot bed levels and bed level change
-        if not change:
-            p = ax.pcolormesh(y, x, zb[time_index,:,:], cmap=cmap_DIFD, vmin=-7.5, vmax=7.5)
-            cb = fig.colorbar(p, shrink=.7)
-            cb.set_label('bed level [m]')
         else:
-            pickup = pickup.sum(axis=-1)             # sum over fractions
-            pickup = np.cumsum(pickup, axis=0)       # cummulative sum in time
-            dz = -pickup / (2650. * .6)               # convert from kg/m2 to m3/m2
-            p = ax.pcolormesh(y, x, dz[time_index,:,:], cmap='bwr_r', vmin=-2, vmax=2)
-            cb = fig.colorbar(p, shrink=.7)
-            cb.set_label('bed level change [m]')
-
-        ax.contour(y, x, zb[0,:,:], [0.], colors=['k'])
-            
-        ax.set_aspect('equal', adjustable='box')
-        ax.invert_yaxis()
-            
-        ax.set_xlabel('alongshore distance [m]')
-        ax.set_ylabel('cross-shore distance [m]')
-
+            p1 = ax.plot(x, zb[time_index_start,:]-zb[time_index_end,:], 'k-', label='Difference')
+            ax.title('Elevation difference initial - final profile')
+            ax.set_xlabel('Cross-shore distance [m]')
+            ax.set_ylabel('Elevation difference [m]')            
+        
+        
     return ax
 
-
-def plot_erosion(ncfile, figsize=(10,4), ax=None):
-    '''Plot erosion in time from an AeoLiS result file
+def plot_flux(ncfile, cum = True,figsize=(10,5), time_index_start=0, time_index_end=-1,grid_cell=442, ax=None):
+    '''Plot cumulated sediment flux from an AeoLiS result file
     
     Parameters
     ----------
     ncfile : str
       Path to AeoLiS result file
+    cum : bool
+      Plot cumulative flux
     figsize : 2-tuple, optional
       Dimensions of resulting figure
+    time_index_start : int
+      Index of first (initial) time dimension to plot [default: 0]
+    time_index_stop : int
+      Index of last (final) time dimension to plot [default: -1]
+    grid_cell : int
+      Index of gridcell to plot [default: 442; where the dike starts]
     ax : matplotlib.axes.SubplotAxis, optional
       Axis used for plotting, if not given a new figure is created
       
     Returns
     -------
     ax : matplotlib.axes.SubplotAxis
-      plot axes object
+      plot axes objects
       
     '''
-
-    name = os.path.split(ncfile)[1]
     
     with netCDF4.Dataset(ncfile, 'r') as ds:
-
-        # get spatial dimensions, time and pickup
-        x = ds.variables['x'][:,:]
-        y = ds.variables['y'][:,:]
+        
+        # get model output
+        qs = np.squeeze(ds.variables['qs'][:,:,grid_cell])
         t = ds.variables['time'][:]
-        pickup = ds.variables['pickup_sum'][...]
         
-        dx = np.diff(x[0,:])[0] # assume equidistant grid
-        dy = np.diff(y[:,0])[0] # assume equidistant grid
+        #constants
+        d = 2650 # Density sediement [kg/m3]
+        p = 0.4 # Porosity [-]
+        delta_t = t[1]-t[0] # Timestep [s]
+        
+        # Calculate volumetric flux [m3/m/s]
+        volflux=(qs*delta_t)/(d*(1-p))
+        cumvol=np.cumsum(volflux)
         
         # create figure
         if ax is None:
-            fig, ax = plt.subplots(1, 1, figsize=figsize)
+            fig, ax = plt.subplots(figsize=figsize)
         
-        # convert pickup to volumes
-        pickup = pickup.sum(axis=-1)             # sum over fractions
-        pickup = np.cumsum(pickup, axis=0)       # cummulative sum in time
-        pickup = pickup * dx * dy / (2650. * .6) # convert from kg/m2 to m3
+        # plot flux
+        if not cum:
+            p1 = ax.plot(t/86400, volflux, 'k-', label='Volumetric flux')
+            plt.title('Volumetric flux')
+            ax.set_xlabel('Time [days]')
+            ax.set_ylabel('Volumetric flux [m$^3$/m/s]')
+            ax.legend()
+
+
+        else:
+            p1 = ax.plot(t/86400, cumvol, 'k-', label='Cumulative volumetric flux')
+            plt.title('Cumulative volumetric flux')
+            ax.set_xlabel('Time [days]')
+            ax.set_ylabel('Cumulative volumetric flux [m$^3$/m]')
+            ax.legend()          
         
-        # determine erosion and deposition and integrat in space
-        E = np.maximum(0., pickup).sum(axis=-1).sum(axis=-1) * 1e-6
-        D = np.minimum(0., pickup).sum(axis=-1).sum(axis=-1) * 1e-6
         
-        # plot erosion in time
-        dt = netCDF4.num2date(t, ds.variables['time'].units,only_use_cftime_datetimes=False)
-        ax.plot(dt, E, label=name)
-        
-        # set titles and labels
-        ax.set_xlabel('time')
-        ax.set_ylabel('erosion volume [$\mathdefault{Mm^3}$]')
-        ax.set_title(name)
-        
-        ax.grid(True)
-            
     return ax
 
-
-def plot_erosion_multi(ncfiles, ax=None, **kwargs):
-    '''Plot erosion in time from multiple AeoLiS result files
+def plot_flux_compare(ncfile_orig, ncfile_new, cum = True,figsize=(10,5), time_index_start=0, time_index_end=-1,grid_cell=442, ax=None):
+    '''Plot cumulated sediment flux from an AeoLiS result file
     
     Parameters
     ----------
-    ncfiles : str
-      Path to AeoLiS result files, may contain wildcards (*)
-    
-    See ``plot_erosion`` for options and return values.
-    
-    See Also
-    --------
-    plot_erosion
-    
+    ncfile_orig : str
+      Path to AeoLiS result file - simulation without moisture processes
+    ncfile_new : str
+      Path to AeoLiS result file - simulation with moisture processes      
+    cum : bool
+      Plot cumulative flux
+    figsize : 2-tuple, optional
+      Dimensions of resulting figure
+    time_index_start : int
+      Index of first (initial) time dimension to plot [default: 0]
+    time_index_stop : int
+      Index of last (final) time dimension to plot [default: -1]
+    grid_cell : int
+      Index of gridcell to plot [default: 442; where the dike starts]
+    ax : matplotlib.axes.SubplotAxis, optional
+      Axis used for plotting, if not given a new figure is created
+      
+    Returns
+    -------
+    ax : matplotlib.axes.SubplotAxis
+      plot axes objects
+      
     '''
-    print(glob.glob(ncfiles))
-    for ncfile in glob.glob(ncfiles):      
-        ax = plot_erosion(ncfile, ax=ax, **kwargs)
-    ax.legend(loc='upper left')
-    ax.set_title('')
-            
+    
+    with netCDF4.Dataset(ncfile_orig, 'r') as ds:
+        
+        # get model output
+        qs = np.squeeze(ds.variables['qs'][:,:,grid_cell])
+        t = ds.variables['time'][:]
+        
+    with netCDF4.Dataset(ncfile_new, 'r') as ds:
+        
+        # get model output
+        qs_new = np.squeeze(ds.variables['qs'][:,:,grid_cell])
+        t_new = ds.variables['time'][:]
+        
+        #constants
+        d = 2650 # Density sediement [kg/m3]
+        p = 0.4 # Porosity [-]
+        delta_t = t[1]-t[0] # Timestep [s]
+        delta_t_new = t_new[1]-t_new[0]
+        
+        # Calculate volumetric flux [m3/m/s]
+        volflux=(qs*delta_t)/(d*(1-p))
+        cumvol=np.cumsum(volflux)
+        
+        volflux_new=(qs_new*delta_t)/(d*(1-p))
+        cumvol_new=np.cumsum(volflux_new)
+        
+        # create figure
+        if ax is None:
+            fig, ax = plt.subplots(figsize=figsize)
+        
+        # plot flux
+        if not cum:
+            p1 = ax.plot(t/86400, volflux, 'k-', label='Volumetric flux - no moisture')
+            p2 = ax.plot(t_new/86400, volflux_new, 'r-', label='Volumetric flux - with moisture')
+            plt.title('Volumetric flux')
+            ax.set_xlabel('Time [days]')
+            ax.set_ylabel('Volumetric flux [m$^3$/m/s]')
+            ax.legend()
+
+
+        else:
+            p1 = ax.plot(t/86400, cumvol, 'k-', label='Volumetric flux - no moisture')
+            p2 = ax.plot(t_new/86400, cumvol_new, 'r-', label='Volumetric flux - with moisture')
+            plt.title('Cumulative volumetric flux')
+            ax.set_xlabel('Time [days]')
+            ax.set_ylabel('Cumulative volumetric flux [m$^3$/m]')
+            ax.legend()          
+        
+        
     return ax
 
 
-def plot_coverage(ncfile, figsize=(10,4), ax=None):
-    '''Plot coverage of non-erodible elements in top layer of bed from an AeoLiS result file
+def plot_moisture_ave(ncfile, figsize=(10,5), ax=None):
+    '''Plot average moisture content across profile from an AeoLiS result file
     
     Parameters
     ----------
@@ -249,56 +223,45 @@ def plot_coverage(ncfile, figsize=(10,4), ax=None):
     Returns
     -------
     ax : matplotlib.axes.SubplotAxis
-      plot axes object
+      plot axes objects
       
     '''
-
-    name = os.path.split(ncfile)[1]
-
+    
     with netCDF4.Dataset(ncfile, 'r') as ds:
         
-        # get spatial dimensions and bed mass
-        x = ds.variables['x'][:,:]
-        y = ds.variables['y'][:,:]
-        zb = ds.variables['zb'][0,:,:]
-        mass = ds.variables['mass'][-1,:,:,:,:]
+        # get spatial dimensions and bed levels
+        x = np.squeeze(ds.variables['x'][:,:])
+        zb = np.squeeze(ds.variables['zb'][...])
+        moist = np.squeeze(ds.variables['moist'][...])
+        moist_ave = np.average(moist,axis=0)
 
-        # compute coverage of largest fraction
-        if mass.ndim < 4 or mass.shape[-1] < 2:
-            coverage = np.ones(x.shape)
-        else:
-            coverage = mass[:,:,0,-1] / mass[:,:,0,:].sum(axis=-1)
-        
         # create figure
         if ax is None:
-            fig, ax = plt.subplots(1, 1, figsize=figsize)
+            fig, ax = plt.subplots(figsize=figsize)
         
-        # plot bed levels and bed level change
-        p = ax.pcolormesh(y, x, coverage, cmap='Reds', vmin=0, vmax=1)
-        ax.contour(y, x, zb, [0.], colors=['k'])
+        # plot bed levels and average moisture content
+        p1 = plt.plot(x[:], zb[0,:],'k-')
+        p2 = plt.scatter(x[:], zb[0,:], c=moist_ave[:], cmap = 'coolwarm_r')
+        cbar = plt.colorbar()    
+        cbar.set_label('Average moisture content [m$^3$/m$^3$]') 
+        plt.title('Average moisture content')
+        ax.set_xlabel('Cross-shore distance [m]')
+        ax.set_ylabel('Elevation rel MSL [m]')
+        plt.grid(True)
         
-        # create colorbars
-        cb = fig.colorbar(p, shrink=.7)
-        cb.set_label('coverage [%]')
-    
-        ax.set_aspect('equal', adjustable='box')
-        ax.invert_yaxis()
-        ax.set_xlabel('alongshore distance [m]')
-        ax.set_ylabel('cross-shore distance [m]')
-        ax.set_title(name)
-
+        
     return ax
 
 
-def create_animation(ncfile, figsize=(10,4), ext='mp4', nframes=None):
-    '''Create animation from an AeoLiS result file
-
+def moisture_animation(ncfile, figsize=(10,5), ext='mp4', nframes=168):
+    '''Animate the groundwater level and surface moisture content across profile from an AeoLiS result file
+    
     Parameters
     ----------
     ncfile : str
       Path to AeoLiS result file
     figsize : 2-tuple, optional
-      Dimensions of resulting video
+      Dimensions of resulting figure
     ext : str, optional
       Extension of resulting video file [default: mp4]
     nframes : int, optional
@@ -310,39 +273,40 @@ def create_animation(ncfile, figsize=(10,4), ext='mp4', nframes=None):
       Path to video file
       
     '''
-
-
+    
     fig, ax = plt.subplots(figsize=figsize)
-    ax.invert_yaxis()
+
     
     with netCDF4.Dataset(ncfile, 'r') as ds:
         t = ds.variables['time'][:]
-        x = ds.variables['x'][:,:]
-        y = ds.variables['y'][:,:]
-        zb = ds.variables['zb'][:,:,:]
+        x = np.squeeze(ds.variables['x'][:,:])
+        zb = np.squeeze(ds.variables['zb'][...])
+        moist = np.squeeze(ds.variables['moist'][...])
+        gw =  np.squeeze(ds.variables['gw'][...])
         units = ds.variables['time'].units
         
     def update(i):
         ax.clear()
-        p = ax.pcolormesh(y, x, zb[i,:,:], cmap=cmap_DIFD, vmin=-7.5, vmax=7.5)
-        ax.set_aspect('equal', adjustable='box')
-        ax.invert_yaxis()
-        ax.set_xlabel('alongshore [m]')
-        ax.set_ylabel('cross-shore [m]')
-        ax.set_title(netCDF4.num2date(t[i], units))
-        return p
+        p1 = ax.plot(x[:],gw[i,:], 'b-')
+        p2 = plt.plot(x[:], zb[i,:],'k-')
+        p3 = plt.scatter(x[:], zb[i,:], c=moist[i,:], cmap = 'coolwarm_r', vmin = 0, vmax = 0.4)
+        ax.set_xlabel('Cross-shore distance [m]')
+        ax.set_ylabel('Elevation rel MSL [m]')
+        ax.set_title('Time =' + str(int(t[i]/3600)) + ' h')
+        return p1,p2,p3
 
     
     if nframes is None:
         nframes = zb.shape[0]
     nframes = int(np.minimum(zb.shape[0], nframes))
     
-    p = update(0)
-    cb = fig.colorbar(p, shrink=.7)
-    cb.set_label('bed level [m]')
-
+    h = update(0)[2]
+    # create colorbar
+    cb = fig.colorbar(h)
+    cb.set_label('Average moisture content [m$^3$/m$^3$]') 
+    
     videofile = '%s.%s' % (os.path.splitext(ncfile)[0], ext)
-    anim = FuncAnimation(fig, update, frames=nframes, interval=30)
+    anim = FuncAnimation(fig, update, frames=nframes, interval=100)
     anim.save(videofile, dpi=150, writer='ffmpeg')
 
     return videofile
